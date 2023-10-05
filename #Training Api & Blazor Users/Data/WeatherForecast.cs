3f4aebb9d32bcc97c7fab0f97a88d91a30e22055
Luca LAURIA <luca.lauria@alten.it>
2023-10-05 15:06:49 +0200

    first commit

---
+++ #Training Api & Blazor Users/Data/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace _Training_Api___Blazor_Users.Data
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
