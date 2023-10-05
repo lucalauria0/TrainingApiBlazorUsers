eb080f7499a686551e24331f46bcd16aa7d5300e
Luca Lauria <luca.lauria@alten.it>
2023-10-05 15:29:04 +0200

    Add Services: ClientManager , Global

---
+++ #Training Api & Blazor Users/Data/Services/ClientManager.cs
@@ -0,0 +1,174 @@
+﻿
+using System.Net.Http.Json;
+using Newtonsoft.Json;
+using RisorseCondivise;
+
+namespace TaskFormUtenteGenericoBlazorApp.Data.Services
+{
+    public class ClientManager
+    {
+        public string baseUrl = string.Empty;
+        public LogService log;
+        public Global g;
+        static HttpClient client = new HttpClient();
+
+        public ClientManager(string urlWebApi = @"https://localhost:7291", LogService logger = null)
+        {
+            baseUrl = urlWebApi;
+            log = logger;
+            g = new Global(logger);
+            log.Info("ApiHelper", "Logs ON");
+        }
+
+     
+
+        public async Task<(Errore?, List<Persona>)> GetPersone(Dictionary<string, List<object>> attributi)
+        {
+            
+            Errore? er = null;
+            List<Persona> retVal = new List<Persona>();
+            string caller = "ApiHelper.GetPersone()";
+            string result = string.Empty;
+
+            try
+            {
+                WebBox box = new WebBox { Attributi = attributi };
+
+                (er, result) = await g.HandleCLient(box, baseUrl, "api/User", "GetUsers");
+                if (er != null)
+                    return (er, retVal);
+                
+                log.Debug(caller, $"Result = {result}");
+
+                (er, retVal) = JsonConvert.DeserializeObject <(Errore?, List<Persona>)>(result, new JsonSerializerSettings() { ObjectCreationHandling = ObjectCreationHandling.Replace});
+            }
+            catch (Exception ex)
+            {
+                er = new Errore(ex);
+                return (er, retVal);
+            }
+            return (er, retVal);
+        }
+
+        public async Task<Errore> Delete(Dictionary<string, List<object>> attributi)
+        {
+
+            Errore er = null;
+            string caller = "ApiHelper.Delete()";
+            string result = string.Empty;
+
+            try
+            {
+                WebBox box = new WebBox { Attributi = attributi };
+
+                (er, result) = await g.HandleCLient(box, baseUrl, "api/User", "Delete");
+                if (er != null)
+                    return (er);
+
+                log.Debug(caller, $"Result = {result}");
+                (er) = JsonConvert.DeserializeObject<Errore>(result, new JsonSerializerSettings() { ObjectCreationHandling = ObjectCreationHandling.Replace });
+            }
+            catch (Exception ex)
+            {
+                er = new Errore(ex);
+                return (er);
+            }
+            return (er);
+        }
+
+        public async Task<Errore> InsertPersona(Persona p)
+        {
+            Errore er = null;
+            string caller = "ApiHelper.InsertPersona()";
+            string result = string.Empty;
+           try
+            {
+
+                log.Debug(caller, $"Persona = {p.ToString()}");
+                WebBox box = new WebBox { Persona = p };
+
+                (er,result) = await g.HandleCLient(box, baseUrl, "api/User", "InsertUser");
+
+                if (er != null)
+                    return (er);
+
+                log.Debug(caller, $"Result = {result}");
+            }
+            catch (Exception e)
+            {
+                er = new Errore(e);
+                log.Error(caller, er);
+                return er;
+            }
+            return er;
+
+        }
+
+        public async Task<Errore> UpdatePersona(Persona p)
+        {
+            Errore er = null;
+            string caller = "ApiHelper.UpdatePersona()";
+            string result = string.Empty;
+            try
+            {
+
+                log.Debug(caller, $"Persona = {p}");
+                WebBox box = new WebBox { Persona = p };
+
+                (er, result) = await g.HandleCLient(box, baseUrl, "api/User", "UpdateUser");
+                if (er != null)
+                    return (er);
+
+                log.Debug(caller, $"Box = {box.Persona.ToString()}");
+                log.Debug(caller, $"Result = {result}");
+            }
+            catch (Exception e)
+            {
+                er = new Errore(e);
+                log.Error(caller, er);
+                return er;
+            }
+            return er;
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="cf"></param>
+        /// <returns></returns>
+        public async Task<Errore> CheckUser(string cf)
+        {
+            Errore er = null;
+            bool result = true;
+            string caller = "Manager, CheckUser()";
+            log.Info(caller, "ON");
+
+            try
+            {
+                Dictionary<string, List<object>> keyValuePairs = new Dictionary<string, List<object>>();
+                keyValuePairs.Add("CodiceFiscale", new List<object> { cf });
+                List<Persona> personas;
+
+                (er, personas) = await GetPersone(keyValuePairs);
+                log.Debug(caller, $"Persone trovate: {personas.Count}");
+
+                if (personas.Any())
+                {
+                    er = new Errore("L'Utente è gia registrato.");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                er = new Errore("Qualcosa è andato storto... :(");
+                log.Error(caller, er);
+            }
+
+            log.Info(caller, "OFF");
+
+            return er;
+        }
+
+    }
+}
---
+++ #Training Api & Blazor Users/Data/Services/Global.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using RisorseCondivise;
+using System.Net.Http.Json;
+
+namespace TaskFormUtenteGenericoBlazorApp.Data
+{
+    public class Global
+    {
+        private LogService log;
+        public Global(LogService service)
+        {
+            log = service;
+        }
+        public async Task<(Errore?, string)> HandleCLient(WebBox box, string destinationUrl, string nomeController, string nomeMetodo)
+        {
+            string risultato = string.Empty;
+            Errore? er = null;
+            try
+            {
+
+
+                HttpClientHandler handler = new HttpClientHandler();
+                handler.UseDefaultCredentials = true;
+
+                using (HttpClient client = new HttpClient(handler))
+                {
+                    client.BaseAddress = new Uri(destinationUrl);
+
+
+                    HttpResponseMessage response = await client.PostAsJsonAsync($"{nomeController}/{nomeMetodo}", JsonConvert.SerializeObject(box));
+                    response.EnsureSuccessStatusCode();
+
+                    risultato = await response.Content.ReadAsStringAsync();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                er = new Errore(ex);
+                log.Error("HandleClient", er);
+            }
+
+            return (er, risultato);
+        }
+        public async Task<(Errore, string)> HandleCLient(StringContent content, string destinationUrl, string nomeController, string nomeMetodo)
+        {
+            Errore er = null;
+            string risultato = string.Empty;
+            try
+            {
+
+
+                HttpClientHandler handler = new HttpClientHandler();
+                handler.UseDefaultCredentials = true;
+
+                using (HttpClient client = new HttpClient(handler))
+                {
+                    client.BaseAddress = new Uri(destinationUrl);
+
+                    HttpResponseMessage response = await client.PostAsync($"{nomeController}/{nomeMetodo}", content);
+                    response.EnsureSuccessStatusCode();
+
+                    risultato = await response.Content.ReadAsStringAsync();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                log.Error("HandleClient", new Errore(ex));
+                er = new Errore(ex);
+            }
+
+            return (er, risultato);
+        }
+    }
+}
